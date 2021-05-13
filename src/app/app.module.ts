52b058fd20cb8bd96df11a8053b450d260f79e7b
livejo <yoseph.kefale@gmail.com>
2021-05-13 14:01:08 +0300

    initial all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import {Routes, RouterModule} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
+import { AddPropertyComponent } from './property/add-property/add-property.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    AddPropertyComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+    PropertyDetailComponent,
+      NavBarComponent
+   ],
   imports: [
     BrowserModule
   ],
---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+    selector: 'app-nav-bar',
+    templateUrl: './nav-bar.component.html',
+    styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+
+    loggedinUser: string;
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    loggedin() {
+        this.loggedinUser = localStorage.getItem('userName');
+        return this.loggedinUser;
+    }
+
+    onLogout() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('userName');
+
+    }
+
+}
