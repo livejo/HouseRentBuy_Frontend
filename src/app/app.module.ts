945011e1ed3dac3a293faa69e13f55300211857c
yosef kefale <yoseph.kefale@gmail.com>
2021-06-01 15:15:07 +0300

    changes 1-6-21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-import {Routes,RouterModule} from '@angular/router';
-import {FormsModule} from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+import { Routes,RouterModule} from '@angular/router';
+import { FormsModule} from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TabsModule } from 'ngx-bootstrap/tabs';
--- src/app/pipes/sort.pipe.ts
+++ src/app/pipes/sort.pipe.ts
@@ -14,6 +14,7 @@ export class SortPipe implements PipeTransform {
       multiplier = -1;
     }
 
+    if (value) {
     value.sort((a: any, b: any) => {
       if (a[sortField] < b[sortField]) {
         return -1 * multiplier;
@@ -27,5 +28,6 @@ export class SortPipe implements PipeTransform {
 
     return value;
   }
+}
 
 }
